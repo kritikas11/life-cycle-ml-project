9c5826f640d263e2272f16d5584c2dfcfb6cee72
kritikas11 <kritikas1107@gmail.com>
2026-02-21 14:46:36 +0530

    Added PORT configuration for deployment

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import pickle
 from flask import Flask, request, render_template
 import numpy as np
+import os
 
 application = Flask(__name__)
 app = application
@@ -43,4 +44,5 @@ def predict_datapoint():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
